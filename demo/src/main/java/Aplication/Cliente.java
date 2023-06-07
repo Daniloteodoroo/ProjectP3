2e189c1e92ac3be4aa80fbd5b28cff3dc823f784
Danilo Teodoro <134449806+DaniloTOliveira@users.noreply.github.com>
2023-06-07 03:22:32 -0300

    Atualizações

--- demo/src/main/java/Aplication/Cliente.java
+++ demo/src/main/java/Aplication/Cliente.java
@@ -1,5 +1,31 @@
 package Aplication;
 
-public class Cliente {
-    
+public class Cliente extends Pessoa {
+    private double valorDevido; 
+
+    public Cliente(String cpf, String endereco, String telefone, double valorDevido, String nome) {
+        super(cpf, endereco, telefone, nome);
+        this.valorDevido = valorDevido;
+    }
+
+    public double getValorDevido() {
+        return valorDevido;
+    }
+
+    public void setValorDevido(double divida) {
+        this.valorDevido = divida;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Cliente that = (Cliente) obj;
+        return that.getCpf().equals(this.getCpf());
+    }
 }
