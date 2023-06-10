e330fe2d6d409138739a3b619aa23bc7510e0550
Danilo Teodoro <134449806+DaniloTOliveira@users.noreply.github.com>
2023-06-10 12:35:46 -0300

    Atualizações

--- demo/src/main/java/Aplication/Cubo.java
+++ demo/src/main/java/Aplication/Cubo.java
@@ -1,5 +1,50 @@
 package Aplication;
-
 public class Cubo {
-    
+    private String nome;
+    private double valor;
+    private int quantidade;
+
+    public Cubo(String nome, double valor, int quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void adicionarQuantidade(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerQuantidade(int quantidade) {
+        if (this.quantidade >= quantidade) {
+            this.quantidade -= quantidade;
+        } else {
+            System.out.println("Não há quantidade suficiente de cubos.");
+        }
+    }
 }
