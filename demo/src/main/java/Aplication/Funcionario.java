25b91ad2acc54f4c4393d262ce898ee6f8e5f44a
Danilo Teodoro <134449806+DaniloTOliveira@users.noreply.github.com>
2023-06-10 12:36:47 -0300

    Atualizações

--- demo/src/main/java/Aplication/Funcionario.java
+++ demo/src/main/java/Aplication/Funcionario.java
@@ -1,5 +1,42 @@
 package Aplication;
+public class Funcionario extends Pessoa {
+    private double salario; 
+    private String ocupacao; 
 
-public class Funcionario {
     
+    public Funcionario(String nome, String cpf, String endereco, String telefone, double salario, String ocupacao) {
+        super(nome, cpf, endereco, telefone);
+        this.salario = salario;
+        this.ocupacao = ocupacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getOcupacao() {
+        return ocupacao;
+    }
+
+    public void setOcupacao(String ocupacao) {
+        this.ocupacao = ocupacao;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Funcionario that = (Funcionario) obj;
+        return that.getCpf().equals(this.getCpf());
+    }
 }
